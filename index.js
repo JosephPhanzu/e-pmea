e5f78bb5deae76a1967e5ac488f978744521d8c8
Joseph Phanzu <gloreensmith@gmail.com>
2024-08-02 19:55:08 +0200

    Modification de la page index

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const app = express();
 
 const connectBdOp = {
     host : process.env.DB_HOST || 'localhost',
-    port : process.env.DB_PORT || 3306,
     database : process.env.DB_NAME || 'e-pmea',
     user : process.env.DB_USER || 'root',
     password : process.env.DB_PASSWORD || ''
